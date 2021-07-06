444d061410461c415199a02553ab4f8e0fd479ad
Matt Venables <matt@venabl.es>
2021-07-06 10:36:27 -0400

    Add per-user manifest flows with QR code

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,28 +1,58 @@
 import { GetServerSideProps, NextPage } from "next"
 import { getSession } from "next-auth/client"
+import QRCode from "qrcode.react"
 import Authenticated, { SessionProps } from "components/Authenticated"
 import Layout from "components/Layout"
+import { User } from "lib/database"
+import { issuanceManifestUrl } from "lib/issuance/manifest"
+import { ManifestUrlObject } from "types/Manifest"
 
-export const getServerSideProps: GetServerSideProps<SessionProps> = async (
+type Props = SessionProps & {
+  manifestUrl: ManifestUrlObject
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
   context
 ) => {
   const session = await getSession(context)
 
+  if (!session || !session.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
+      manifestUrl: await issuanceManifestUrl(session.user as User),
       session
     }
   }
 }
 
-const Protected: NextPage = () => {
+const Protected: NextPage<Props> = ({ manifestUrl }) => {
   return (
     <Authenticated>
       <Layout>
         <main className="container py-4 mx-auto font-inter">
-          <h1 className="text-4xl font-extrabold tracking-tight text-center">
+          <h1 className="pb-8 text-4xl font-extrabold tracking-tight text-center">
             Welcome back!
           </h1>
+
+          <h2 className="pb-4 text-xl text-center">KYC Credential:</h2>
+          <QRCode
+            value={JSON.stringify(manifestUrl)}
+            className="mx-auto w-96 h-96"
+            renderAs="svg"
+          />
+          <textarea
+            className="container mx-auto my-2 font-mono border-2 h-36"
+            readOnly
+            value={JSON.stringify(manifestUrl, null, 4)}
+          />
         </main>
       </Layout>
     </Authenticated>
