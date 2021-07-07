e1e81cecb5fd10e23567e286137edbafb1e6f8a5
Brice Stacey <bricestacey@gmail.com>
2021-07-07 16:11:57 -0400

    Prompt user to request credentials after scanning QR code

--- packages/demo-wallet/components/HomePage.tsx
+++ packages/demo-wallet/components/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import {View, StyleSheet, Button} from "react-native"
+import {View, StyleSheet, Button, Alert} from "react-native"
+import {CredentialManifest} from "../types"
 
 export default function HomePage({navigation}) {
   return (
@@ -8,10 +9,36 @@ export default function HomePage({navigation}) {
         title={"Scan QR Code"}
         onPress={() =>
           navigation.navigate("Scanner", {
-            onScan: ({type, data}) => {
-              alert(
-                `Bar code with type ${type} and data ${data} has been scanned!`
-              )
+            onScan: async ({type, data}) => {
+              // When scanning a QR code, it will encode a JSON object with a
+              // manifestUrl property. We will subsequently fetch that value
+              // to retrieve the full manifest document.
+
+              // Parse manifest URL
+              const manifestUrl = JSON.parse(data).manifestUrl
+
+              // Fetch manifest URL
+              const result = await fetch(manifestUrl)
+
+              // Parse the manifest
+              const manifest: CredentialManifest = await result.json()
+
+              // Parse the styles
+              const outputDescriptor = manifest.output_descriptors[0]
+              const name = outputDescriptor?.name || "Request Credentials"
+              const description = outputDescriptor?.description
+
+              // Prompt user to request the credentials or cancel
+              Alert.alert(name, description, [
+                {
+                  text: "Cancel",
+                  style: "cancel"
+                },
+                {
+                  text: "Request",
+                  onPress: () => Alert.alert("Requesting VC")
+                }
+              ])
             }
           })
         }
