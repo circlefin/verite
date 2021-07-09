f148881add6e19c0ef1dde19683665eaee18933f
Matt Venables <matt@venabl.es>
2021-07-09 11:52:04 -0400

    Add polyfill for TextEncoder/TextDecoder

--- packages/demo-wallet/index.js
+++ packages/demo-wallet/index.js
@@ -1,8 +1,12 @@
 import "./shim.js"
 import "crypto"
 import "react-native-gesture-handler"
+import { TextEncoder, TextDecoder } from "fastestsmallesttextencoderdecoder"
 import { AppRegistry } from "react-native"
+import { polyfillGlobal } from "react-native/Libraries/Utilities/PolyfillFunctions"
 import App from "./App"
 import { name as appName } from "./app.json"
 
+polyfillGlobal("TextEncoder", () => TextEncoder)
+polyfillGlobal("TextDecoder", () => TextDecoder)
 AppRegistry.registerComponent(appName, () => App)
