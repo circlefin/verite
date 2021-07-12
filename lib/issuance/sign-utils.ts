d0ca34bb914e3a2e92668431b47a9cb4085df407
Matt Venables <matt@venabl.es>
2021-07-12 09:07:10 -0400

    Update Issuer env variables

--- lib/issuance/sign-utils.ts
+++ lib/issuance/sign-utils.ts
@@ -1,18 +1,20 @@
-import { EdDSASigner } from 'did-jwt'
-import { Issuer as DidJwtIssuer, createVerifiableCredentialJwt } from 'did-jwt-vc'
+import { EdDSASigner } from "did-jwt"
+import {
+  Issuer as DidJwtIssuer,
+  createVerifiableCredentialJwt
+} from "did-jwt-vc"
+import { JWT } from "did-jwt-vc/lib/types"
+
+const did = process.env.ISSUER_DID
+const secret = process.env.ISSUER_SECRET
+
+export const issuer: DidJwtIssuer = {
+  did: did,
+  alg: "EdDSA",
+  signer: EdDSASigner(secret)
+}
 
 // TODO(kim): fix types
-export const signVc = async (vcPayload: any): Promise<any> => {
-  const did = process.env.ISSUER_DID
-  const secret = process.env.ISSUER_SECRET
-
-  const myIssuer : DidJwtIssuer = {
-    did: did,
-    alg: "EdDSA",
-    signer: EdDSASigner(
-      secret
-    )
-  }
-
-  return createVerifiableCredentialJwt(vcPayload, myIssuer)
+export const signVc = async (vcPayload: any): Promise<JWT> => {
+  return createVerifiableCredentialJwt(vcPayload, issuer)
 }
