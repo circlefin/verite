e2fb623fbc985dfee1e72144a1132b6d9c4795b2
Brice Stacey <bricestacey@gmail.com>
2021-07-12 17:54:02 -0400

    Add DID information to Settings tab

--- packages/demo-wallet/src/components/SettingsScreen.tsx
+++ packages/demo-wallet/src/components/SettingsScreen.tsx
@@ -1,17 +1,95 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React, { useState, useEffect } from "react"
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+  useColorScheme,
+  Text
+} from "react-native"
+import { Colors } from "react-native/Libraries/NewAppScreen"
 
-const SettingsScreen = () => {
+import { getOrCreateDidKey, Did } from "../lib/storage"
+
+const Section: React.FC<{
+  title: string
+}> = ({ children, title }) => {
+  const isDarkMode = useColorScheme() === "dark"
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "center"
-      }}>
-      <Text style={{ textAlign: "center" }}>Settings go here</Text>
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black
+          }
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark
+          }
+        ]}>
+        {children}
+      </Text>
     </View>
   )
 }
+
+const SettingsScreen = () => {
+  const [did, setDid] = useState<Did | null>(null)
+
+  useEffect(() => {
+    ;(async () => {
+      const didKey = await getOrCreateDidKey()
+      setDid(didKey)
+    })()
+  }, [])
+
+  if (!did) {
+    return <View />
+  }
+
+  const publicKey = Buffer.from(did.publicKey).toString("hex")
+  const privateKey = Buffer.from(did.privateKey).toString("hex")
+
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Section title="Id">
+          <Text style={styles.sectionDescription}>{did.id}</Text>
+        </Section>
+        <Section title="Controller">
+          <Text style={styles.sectionDescription}>{did.controller}</Text>
+        </Section>
+        <Section title="Public Key">
+          <Text style={styles.sectionDescription}>{publicKey}</Text>
+        </Section>
+        <Section title="Private Key">
+          <Text style={styles.sectionDescription}>{privateKey}</Text>
+        </Section>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "600"
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: "400"
+  }
+})
 
 export default SettingsScreen
