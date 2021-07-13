84e89cd912c926aa57fe20414ff6dfb0b9881c03
Brice Stacey <bricestacey@gmail.com>
2021-07-13 15:45:56 -0400

    Add additional data to Credentials List

--- packages/demo-wallet/src/components/CredentialsList.tsx
+++ packages/demo-wallet/src/components/CredentialsList.tsx
@@ -1,12 +1,30 @@
+import compact from "lodash/compact"
 import React, { useState, useEffect } from "react"
-import { FlatList, Text, TouchableOpacity } from "react-native"
+import { FlatList, Text, TouchableOpacity, View } from "react-native"
+import Ionicons from "react-native-vector-icons/Ionicons"
+import { asyncMap } from "../lib/async-fns"
 import { getCredentials } from "../lib/storage"
+import { decodeVc } from "../lib/verity"
 
 const CredentialsList = ({ navigation }) => {
   const [credentials, setCredentials] = useState([])
   useEffect(() => {
     ;(async () => {
-      setCredentials(await getCredentials())
+      const creds = await getCredentials()
+      const doo = await asyncMap(creds, async cred => {
+        const vc = await decodeVc(cred.verifiableCredential[0])
+        const attestation =
+          vc?.verifiableCredential.credentialSubject.KYCAMLAttestation
+        return {
+          id: cred.credential_fulfillment.id,
+          authorityId: attestation.authorityId,
+          authorityName: attestation.authorityName,
+          authorityUrl: attestation.authorityUrl,
+          raw: vc,
+          cred
+        }
+      })
+      setCredentials(doo)
     })()
   }, [credentials])
 
@@ -15,13 +33,29 @@ const CredentialsList = ({ navigation }) => {
   }
 
   const renderItem = ({ item }) => {
+    console.log(item)
     return (
       <TouchableOpacity
         style={{
           margin: 16
         }}
-        onPress={() => navigation.navigate("Details", { credential: item })}>
-        <Text>{item.credential_fulfillment.id}</Text>
+        onPress={() =>
+          navigation.navigate("Details", { credential: item.cred })
+        }>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={{ paddingRight: 16 }}>
+            <Ionicons name={"finger-print"} size={32} />
+          </View>
+
+          <Text>
+            {compact([
+              item.id,
+              item.authorityName,
+              item.authorityUrl,
+              item.authorityId
+            ]).join("\n")}
+          </Text>
+        </View>
       </TouchableOpacity>
     )
   }
@@ -30,7 +64,7 @@ const CredentialsList = ({ navigation }) => {
     <FlatList
       data={credentials}
       renderItem={renderItem}
-      keyExtractor={item => item.credential_fulfillment.id}
+      keyExtractor={item => item.id}
     />
   )
 }
