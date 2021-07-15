36f339118b7d29312f359e6c6b6117715d214586
Matt Venables <matt@venabl.es>
2021-07-15 15:18:50 -0400

    Add attestation navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -130,27 +130,29 @@ const Header: FC<Props> = ({ title }) => {
                 <div className="px-2 pt-2 pb-3 space-y-1">
                   {session &&
                     navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={activeClass(
-                          item.href,
-                          "bg-gray-900 text-white",
-                          "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "block px-3 py-2 rounded-md text-base font-medium"
-                        )}
-                      >
-                        {item.name}
-                      </a>
+                      <Link key={item.name} href={item.href}>
+                        <a
+                          className={activeClass(
+                            item.href,
+                            "bg-gray-900 text-white",
+                            "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "block px-3 py-2 rounded-md text-base font-medium"
+                          )}
+                        >
+                          {item.name}
+                        </a>
+                      </Link>
                     ))}
                 </div>
               </Disclosure.Panel>
             </>
           )}
         </Disclosure>
-        <header className="py-10">
+        <header className="py-4 sm:py-10">
           <div className="max-w-3xl px-4 mx-auto sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-white">{title}</h1>
+            <h1 className="text-2xl font-bold text-white sm:text-3xl">
+              {title}
+            </h1>
           </div>
         </header>
       </div>
--- components/issuer/AttestationNavigation.tsx
+++ components/issuer/AttestationNavigation.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useActiveClass } from "lib/react-fns"
+
 const tabs = [
   { name: "KYC/AML", href: "/attestations/kyc" },
   { name: "Credit Score", href: "/attestations/credit-score" }
@@ -8,44 +12,27 @@ function classNames(...classes) {
 }
 
 export default function AttestationNavigation() {
+  const router = useRouter()
+  const activeClass = useActiveClass(router)
+
   return (
-    <div>
-      <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
-          Select a tab
-        </label>
-        <select
-          id="tabs"
-          name="tabs"
-          className="block w-full py-2 pl-3 pr-10 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          // defaultValue={tabs.find((tab) => tab.current).name}
-        >
-          {tabs.map((tab) => (
-            <option key={tab.name}>{tab.name}</option>
-          ))}
-        </select>
-      </div>
-      <div className="hidden sm:block">
-        <div className="border-b border-gray-200">
-          <nav className="flex -mb-px space-x-8" aria-label="Tabs">
-            {tabs.map((tab) => (
-              <a
-                key={tab.name}
-                href={tab.href}
-                className={classNames(
-                  tab.current
-                    ? "border-indigo-500 text-indigo-600"
-                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                  "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
-                )}
-                aria-current={tab.current ? "page" : undefined}
-              >
-                {tab.name}
-              </a>
-            ))}
-          </nav>
-        </div>
-      </div>
+    <div className="mb-6 -mt-6 border-b border-gray-200">
+      <nav className="flex -mb-px space-x-8" aria-label="Tabs">
+        {tabs.map((tab) => (
+          <Link key={tab.name} href={tab.href}>
+            <a
+              className={activeClass(
+                tab.href,
+                "border-indigo-500 text-indigo-600",
+                "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
+                "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+              )}
+            >
+              {tab.name}
+            </a>
+          </Link>
+        ))}
+      </nav>
     </div>
   )
 }
