5f88882b03fa7dd8386081daa989b0afc6b27083
Matt Venables <matt@venabl.es>
2021-07-20 16:09:37 -0400

    Validate verification submissions

--- pages/api/verification/responses.ts
+++ pages/api/verification/responses.ts
@@ -1,6 +1,20 @@
 import { NextApiHandler } from "next"
+import { methodNotAllowed, validationError } from "lib/api-fns"
+import { validateVerificationSubmission } from "lib/verification/submission"
+import { VerificationSubmission } from "lib/verity"
 
-const handler: NextApiHandler = (req, res) => {
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    return methodNotAllowed(res)
+  }
+
+  const submission: VerificationSubmission = req.body
+  try {
+    await validateVerificationSubmission(submission)
+  } catch (err) {
+    return validationError(res, err)
+  }
+
   res.json({ status: "ok" })
 }
 
