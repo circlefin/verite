991c96effe2c0fde39c0dd4d8d2afd698e74db11
Matt Venables <matt@venabl.es>
2021-07-28 05:45:31 -0400

    Show success on verifier page upon successful verification

--- packages/demo-site/pages/verifier/index.tsx
+++ packages/demo-site/pages/verifier/index.tsx
@@ -1,12 +1,20 @@
+import { BadgeCheckIcon, XCircleIcon } from "@heroicons/react/outline"
 import { GetServerSideProps, NextPage } from "next"
 import QRCode from "qrcode.react"
+import { useState } from "react"
+import useSWR from "swr"
 import VerifierLayout from "components/verifier/Layout"
 import { saveVerificationRequest } from "lib/database"
 import { generateKycVerificationRequest } from "lib/verification/requests"
-import { VerificationRequestWrapper } from "types"
+
+export type VerificationRequestWrapper = {
+  requestUrl: string
+  version: string
+}
 
 type Props = {
   verificationRequestWrapper: VerificationRequestWrapper
+  id: string
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
@@ -20,24 +28,53 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
 
   return {
     props: {
+      id: verificationRequest.request.id,
       verificationRequestWrapper
     }
   }
 }
 
-const VerifierPage: NextPage<Props> = ({ verificationRequestWrapper }) => {
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+function QRCodeOrStatus({
+  id,
+  verificationRequestWrapper
+}: Props): JSX.Element {
+  const { data } = useSWR(`/api/verification/${id}/status`, fetcher, {
+    refreshInterval: 1000
+  })
+
+  if (data) {
+    if (data.status === "approved") {
+      return <BadgeCheckIcon className="w-48 h-48 mx-auto text-green-400" />
+    } else if (data.status === "rejected") {
+      return <XCircleIcon className="w-48 h-48 mx-auto text-red-400" />
+    }
+  }
+
+  return (
+    <>
+      <QRCode
+        value={JSON.stringify(verificationRequestWrapper)}
+        className="w-48 h-48 mx-auto"
+        renderAs="svg"
+      />
+      <textarea
+        className="container h-40 mx-auto font-mono text-sm border-2"
+        readOnly
+        value={JSON.stringify(verificationRequestWrapper, null, 4)}
+      />
+    </>
+  )
+}
+
+const VerifierPage: NextPage<Props> = ({ id, verificationRequestWrapper }) => {
   return (
     <VerifierLayout title="KYC/AML Verification">
       <div className="flex flex-col justify-center space-y-8">
-        <QRCode
-          value={JSON.stringify(verificationRequestWrapper)}
-          className="w-48 h-48 mx-auto"
-          renderAs="svg"
-        />
-        <textarea
-          className="container h-40 mx-auto font-mono text-sm border-2"
-          readOnly
-          value={JSON.stringify(verificationRequestWrapper, null, 4)}
+        <QRCodeOrStatus
+          id={id}
+          verificationRequestWrapper={verificationRequestWrapper}
         />
       </div>
     </VerifierLayout>
