d06981c94014b1b72e87c5f370e10e392f8386fb
Matt Venables <matt@venabl.es>
2021-07-30 15:37:22 -0400

    Use custom revocation list urls

--- packages/demo-site/pages/api/revocation/[[...id]].ts
+++ packages/demo-site/pages/api/revocation/[[...id]].ts
@@ -3,7 +3,7 @@ import { apiHandler, notFound } from "../../../lib/api-fns"
 import { getRevocationListById } from "../../../lib/database"
 
 export default apiHandler<RevocationListCredential>(async (req, res) => {
-  const q = process.env.REVOCATION_URL
+  const q = `${process.env.HOST}${req.url}`
   const revocationList = await getRevocationListById(q)
 
   if (!revocationList) {
