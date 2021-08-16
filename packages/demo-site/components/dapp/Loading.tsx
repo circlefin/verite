95396b6d4d8e890be1a5f552882083824f3ab8ae
Matt Venables <matt@venabl.es>
2021-08-16 09:25:02 -0400

    Add basic spinner for loading states

--- packages/demo-site/components/dapp/Loading.tsx
+++ packages/demo-site/components/dapp/Loading.tsx
@@ -1,37 +1,12 @@
 import { FC } from "react"
+import Spinner from "../Spinner"
 
 const Loading: FC = () => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        zIndex: 2,
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: "rgba(255, 255, 255, 0.5)",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          zIndex: 3,
-          top: "50%",
-          left: "50%",
-          width: "100px",
-          height: "50px",
-          marginLeft: "-50px",
-          marginTop: " -25px",
-          textAlign: "center",
-        }}
-      >
-        <div className="spinner-border" role="status">
-          <span className="sr-only">*</span>
-        </div>
-      </div>
+    <div className="flex items-center justify-center pt-20">
+      <Spinner className="w-12 h-12" />
     </div>
   )
 }
 
-export default Loading;
+export default Loading
