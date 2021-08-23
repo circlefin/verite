3d65a576b35ed4fee99be3348847c80500abbfb6
Brice Stacey <bricestacey@gmail.com>
2021-08-23 17:37:35 -0400

    Add loading indicator to the faucet button

--- packages/demo-site/components/dapp/NoTokensMessage.tsx
+++ packages/demo-site/components/dapp/NoTokensMessage.tsx
@@ -1,5 +1,6 @@
 import { PlusIcon } from "@heroicons/react/outline"
 import { FC, useState } from "react"
+import DotLoader from "react-spinners/DotLoader"
 
 export type NoTokensMessageProps = {
   faucetFunction: (address: string) => Promise<boolean>
@@ -32,7 +33,18 @@ const NoTokensMessage: FC<NoTokensMessageProps> = ({
             loading ? "opacity-50" : ""
           } inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
         >
-          <PlusIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
+          {loading ? (
+            <DotLoader
+              color="#FFF"
+              size={20}
+              css={`
+                margin-left: -0.25rem;
+                margin-right: 0.5rem;
+              `}
+            />
+          ) : (
+            <PlusIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
+          )}
           Get Tokens
         </button>
       </p>
