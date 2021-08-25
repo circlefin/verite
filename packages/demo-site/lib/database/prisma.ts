96d73b167a48a077c233d9bbd837bdd1ca671603
Matt Venables <matt@venabl.es>
2021-08-25 14:30:56 -0400

    Re-use prisma instances locally

--- packages/demo-site/lib/database/prisma.ts
+++ packages/demo-site/lib/database/prisma.ts
@@ -6,4 +6,13 @@ export type {
   VerificationRequest
 } from "@prisma/client"
 
-export const prisma = new PrismaClient()
+export let prisma: PrismaClient
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
