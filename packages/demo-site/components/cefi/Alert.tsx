5a21de2140408485bec9719c9c01b980f2c9c5f8
Brice Stacey <bricestacey@gmail.com>
2021-08-25 18:03:59 -0400

    More CeFi UX improvements

---
+++ packages/demo-site/components/cefi/Alert.tsx
@@ -0,0 +1,63 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { ExclamationIcon } from "@heroicons/react/outline"
+import { CheckCircleIcon, XIcon } from "@heroicons/react/solid"
+
+export default function Alert({ text, type, onDismiss }) {
+  if (type === "error") {
+    return (
+      <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+        <div className="flex">
+          <div className="flex-shrink-0">
+            <ExclamationIcon
+              className="h-5 w-5 text-yellow-400"
+              aria-hidden="true"
+            />
+          </div>
+          <div className="ml-3">
+            <p className="text-sm text-yellow-700">{text}</p>
+          </div>
+          <div className="ml-auto pl-3">
+            <div className="-mx-1.5 -my-1.5">
+              <button
+                onClick={onDismiss}
+                type="button"
+                className="inline-flex bg-yellow-50 rounded-md p-1.5 text-yellow-500 hover:bg-yellow-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-yellow-50 focus:ring-yellow-600"
+              >
+                <span className="sr-only">Dismiss</span>
+                <XIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="rounded-md bg-green-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <CheckCircleIcon
+            className="h-5 w-5 text-green-400"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm font-medium text-green-800">{text}</p>
+        </div>
+        <div className="ml-auto pl-3">
+          <div className="-mx-1.5 -my-1.5">
+            <button
+              onClick={onDismiss}
+              type="button"
+              className="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
+            >
+              <span className="sr-only">Dismiss</span>
+              <XIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- packages/demo-site/pages/cefi.tsx
+++ packages/demo-site/pages/cefi.tsx
@@ -1,44 +1,107 @@
 import { NextPage } from "next"
-import React, { useState } from "react"
+import { signout, signOut, useSession } from "next-auth/client"
+import React, { createRef, useState } from "react"
 
-import Layout from "../components/layouts/EthLayout"
+import Layout from "../components/Layout"
+import { LoadingButton } from "../components/LoadingButton"
+import Alert from "../components/cefi/Alert"
 import { useBalance } from "../hooks/useBalance"
-import { useEthBalance } from "../hooks/useBalance"
 
-const sendFunction = async (address: string, amount: string) => {
-  await fetch("/api/demo/send", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      transaction: {
-        amount,
-        address
-      }
-    })
-  })
-}
+const form = createRef<HTMLFormElement>()
 
 const Page: NextPage = () => {
+  const [session] = useSession()
   const { balance } = useBalance()
-  const { data } = useEthBalance(balance?.address)
-  const alice = useEthBalance("0xFAEd4F38A7a2628d65699C71be0650FBff4617e6")
+  const [loading, setLoading] = useState<boolean>(false)
+  const [message, setMessage] = useState<{ text: string; type: string }>()
   const [address, setAddress] = useState<string>(
-    "0xFAEd4F38A7a2628d65699C71be0650FBff4617e6"
+    "" // TODO: default address (Alice)
   )
   const [amount, setAmount] = useState<string>("")
 
-  console.log(data)
+  const error = (text: string) => {
+    setMessage({ text, type: "error" })
+  }
+
+  const info = (text: string) => {
+    setMessage({ text, type: "success" })
+  }
+
+  const sendFunction = async (address: string, amount: string) => {
+    // Disable form
+    setLoading(true)
+
+    // Perform send
+    const response = await fetch("/api/demo/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        transaction: {
+          amount,
+          address
+        }
+      })
+    })
+
+    // Enable and reset form
+    setLoading(false)
+
+    return response.status === 200
+  }
+
   return (
     <Layout title="Project Verity Demo">
       <React.StrictMode>
+        <div className="flex flex-col-reverse justify-between mb-6 -mt-6 border-b border-gray-200 sm:flex-row">
+          <nav
+            className="flex justify-center -mb-px space-x-8"
+            aria-label="Tabs"
+          >
+            <span className="px-1 py-4 text-sm font-medium text-gray-500 whitespace-nowrap ">
+              Balance:
+              <span className="ml-3 text-lg font-bold">
+                {balance?.balance} VUSDC
+              </span>
+            </span>
+          </nav>
+          {session ? (
+            <div className="flex justify-between -mb-px space-x-8">
+              <span className="flex items-center px-1 py-4 text-sm font-medium text-gray-500 whitespace-nowrap">
+                {session?.user?.email}
+              </span>
+              <button
+                onClick={() => signout()}
+                className="px-1 py-4 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300 whitespace-nowrap"
+              >
+                Sign Out
+              </button>
+            </div>
+          ) : null}
+        </div>
+
         <div>
+          <div className={`${message ? "block" : "hidden"} mb-4`}>
+            <Alert
+              text={message?.text}
+              type={message?.type}
+              onDismiss={() => setMessage(null)}
+            />
+          </div>
+
           <form
+            ref={form}
             className="space-y-2"
-            onSubmit={(e) => {
+            onSubmit={async (e) => {
               e.preventDefault()
-              sendFunction(address, amount)
+
+              if (await sendFunction(address, amount)) {
+                info("Transfer complete!")
+              } else {
+                error("Transfer failed.")
+              }
+              form.current.reset()
             }}
           >
             <h3 className="text-lg leading-6 font-medium text-gray-900">
@@ -56,13 +119,14 @@ const Page: NextPage = () => {
               </label>
               <div className="mt-1">
                 <input
+                  disabled={loading}
                   type="text"
                   name="address"
                   id="address"
                   className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                  placeholder="0x"
+                  placeholder="0x..."
                   onChange={(e) => setAddress(e.target.value)}
-                  defaultValue={address}
+                  defaultValue="0xFAEd4F38A7a2628d65699C71be0650FBff4617e6"
                 />
               </div>
             </div>
@@ -76,6 +140,7 @@ const Page: NextPage = () => {
               </label>
               <div className="mt-1">
                 <input
+                  disabled={loading}
                   type="text"
                   name="amount"
                   id="amount"
@@ -86,22 +151,15 @@ const Page: NextPage = () => {
               </div>
             </div>
 
-            <button
+            <LoadingButton
               type="submit"
-              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              style="dot-loader"
+              loading={loading}
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
               Transfer
-            </button>
+            </LoadingButton>
           </form>
-        </div>
-
-        <div className="mt-8">
-          <p>Debugger:</p>
-          <div>
-            On-Chain Balance: {data?.balance.toString()} {data?.symbol}
-          </div>
-          <div>Balance: {balance?.balance} VUSDC</div>
-          <div>Alice: {alice?.data?.balance?.toString()} VUSDC</div>
         </div>
       </React.StrictMode>
     </Layout>
