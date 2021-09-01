6c4df31267036439e5e99055485742268eedb755
Matt Venables <matt@venabl.es>
2021-09-01 13:21:57 -0400

    Fix missing return types

--- packages/demo-site/components/cefi/Alert.tsx
+++ packages/demo-site/components/cefi/Alert.tsx
@@ -1,24 +1,30 @@
 import { ExclamationIcon } from "@heroicons/react/outline"
 import { CheckCircleIcon, XIcon } from "@heroicons/react/solid"
 
+type Props = {
+  text: string
+  type: string
+  onDismiss: () => void
+}
+
 /**
  * Simple alert to show success / failure messages
  */
-export default function Alert({ text, type, onDismiss }) {
+export default function Alert({ text, type, onDismiss }: Props): JSX.Element {
   if (type === "error") {
     return (
-      <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+      <div className="p-4 border-l-4 border-yellow-400 bg-yellow-50">
         <div className="flex">
           <div className="flex-shrink-0">
             <ExclamationIcon
-              className="h-5 w-5 text-yellow-400"
+              className="w-5 h-5 text-yellow-400"
               aria-hidden="true"
             />
           </div>
           <div className="ml-3">
             <p className="text-sm text-yellow-700">{text}</p>
           </div>
-          <div className="ml-auto pl-3">
+          <div className="pl-3 ml-auto">
             <div className="-mx-1.5 -my-1.5">
               <button
                 onClick={onDismiss}
@@ -26,7 +32,7 @@ export default function Alert({ text, type, onDismiss }) {
                 className="inline-flex bg-yellow-50 rounded-md p-1.5 text-yellow-500 hover:bg-yellow-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-yellow-50 focus:ring-yellow-600"
               >
                 <span className="sr-only">Dismiss</span>
-                <XIcon className="h-5 w-5" aria-hidden="true" />
+                <XIcon className="w-5 h-5" aria-hidden="true" />
               </button>
             </div>
           </div>
@@ -36,18 +42,18 @@ export default function Alert({ text, type, onDismiss }) {
   }
 
   return (
-    <div className="rounded-md bg-green-50 p-4">
+    <div className="p-4 rounded-md bg-green-50">
       <div className="flex">
         <div className="flex-shrink-0">
           <CheckCircleIcon
-            className="h-5 w-5 text-green-400"
+            className="w-5 h-5 text-green-400"
             aria-hidden="true"
           />
         </div>
         <div className="ml-3">
           <p className="text-sm font-medium text-green-800">{text}</p>
         </div>
-        <div className="ml-auto pl-3">
+        <div className="pl-3 ml-auto">
           <div className="-mx-1.5 -my-1.5">
             <button
               onClick={onDismiss}
@@ -55,7 +61,7 @@ export default function Alert({ text, type, onDismiss }) {
               className="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
             >
               <span className="sr-only">Dismiss</span>
-              <XIcon className="h-5 w-5" aria-hidden="true" />
+              <XIcon className="w-5 h-5" aria-hidden="true" />
             </button>
           </div>
         </div>
--- packages/demo-site/components/cefi/Modal.tsx
+++ packages/demo-site/components/cefi/Modal.tsx
@@ -2,21 +2,33 @@ import { Dialog, Transition } from "@headlessui/react"
 import { ExclamationIcon } from "@heroicons/react/outline"
 import { Fragment, useRef } from "react"
 
+type Props = {
+  confirmFunction: () => void
+  onClose: () => void
+  open: boolean
+  setOpen: (open: boolean) => void
+}
+
 /**
  * Modal used to prompt sender when sending over 10 VUSDC.
  */
-export default function Modal({ confirmFunction, onClose, open, setOpen }) {
+export default function Modal({
+  confirmFunction,
+  onClose,
+  open,
+  setOpen
+}: Props): JSX.Element {
   const cancelButtonRef = useRef(null)
 
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed z-10 inset-0 overflow-y-auto"
+        className="fixed inset-0 z-10 overflow-y-auto"
         initialFocus={cancelButtonRef}
         onClose={onClose}
       >
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -26,7 +38,7 @@ export default function Modal({ confirmFunction, onClose, open, setOpen }) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
           </Transition.Child>
 
           {/* This element is to trick the browser into centering the modal contents. */}
@@ -45,18 +57,18 @@ export default function Modal({ confirmFunction, onClose, open, setOpen }) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+            <div className="inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
               <div className="sm:flex sm:items-start">
-                <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
                   <ExclamationIcon
-                    className="h-6 w-6 text-red-600"
+                    className="w-6 h-6 text-red-600"
                     aria-hidden="true"
                   />
                 </div>
                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg leading-6 font-medium text-gray-900"
+                    className="text-lg font-medium leading-6 text-gray-900"
                   >
                     Share your information
                   </Dialog.Title>
@@ -72,7 +84,7 @@ export default function Modal({ confirmFunction, onClose, open, setOpen }) {
               <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={() => {
                     setOpen(false)
                     confirmFunction()
@@ -82,7 +94,7 @@ export default function Modal({ confirmFunction, onClose, open, setOpen }) {
                 </button>
                 <button
                   type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
                   onClick={() => setOpen(false)}
                   ref={cancelButtonRef}
                 >
