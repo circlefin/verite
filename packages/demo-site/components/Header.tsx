4ebe16eaa8afe1ba89997b2de7cbb7cb6182e4b9
Brice Stacey <bricestacey@gmail.com>
2021-09-07 16:38:05 -0400

    Add red theme for Trapezoids

--- packages/demo-site/components/Header.tsx
+++ packages/demo-site/components/Header.tsx
@@ -1,17 +1,28 @@
+import { useSession } from "next-auth/client"
 import Head from "next/head"
 import { FC } from "react"
 
 export type HeaderProps = {
   title: string
+  theme?: string
 }
 
-const Header: FC<HeaderProps> = ({ title }) => {
+const Header: FC<HeaderProps> = ({ title, theme }) => {
+  const [session] = useSession()
+
+  const circle = ["alice@test.com", "sean@test.com", "matt@test.com"]
+
+  if (circle.includes(session?.user?.email)) {
+    theme = "red"
+  }
   return (
     <>
       <Head>
         <title>Verity.id | {title}</title>
       </Head>
-      <div className="pb-32 bg-blue-600">
+      <div
+        className={`pb-32 ${theme === "red" ? "bg-red-500" : "bg-blue-600"}`}
+      >
         <header className="py-4 sm:py-10">
           <div className="max-w-4xl px-4 mx-auto sm:px-6 lg:px-8">
             <h1 className="text-2xl font-bold text-white sm:text-3xl">
