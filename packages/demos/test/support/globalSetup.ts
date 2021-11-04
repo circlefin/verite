e89ca56401175776675443313be4f27ef8567c3c
Matt Venables <matt@venabl.es>
2021-11-04 16:04:44 -0400

    Remove extra .env files

--- packages/demos/test/support/globalSetup.ts
+++ packages/demos/test/support/globalSetup.ts
@@ -3,7 +3,7 @@ import { loadEnvConfig } from "@next/env"
 
 export default async function loadEnvironmentVariables(): Promise<void> {
   // Load environment variables
-  loadEnvConfig(process.cwd())
+  loadEnvConfig(process.cwd(), true)
 
   // Migrate a database
   execSync("npx prisma migrate reset --force")
