1635819b9fadb535da0d8b7c98fcd2b7eeec17e0
Brice Stacey <bricestacey@gmail.com>
2022-01-27 14:00:13 -0500

    Add better documentation for Solana

--- packages/solana/tests/verity.ts
+++ packages/solana/tests/verity.ts
@@ -2,22 +2,15 @@ import * as anchor from "@project-serum/anchor"
 import { Program } from "@project-serum/anchor"
 import { Verity } from "../target/types/verity"
 import { keccak_256 } from "js-sha3"
-import { privateKeyVerify, ecdsaSign, publicKeyCreate } from "secp256k1"
-import { Secp256k1Program, PublicKey } from "@solana/web3.js"
+import { ecdsaSign } from "secp256k1"
+import { PublicKey } from "@solana/web3.js"
 import * as borsh from "@project-serum/borsh"
 import { expect } from "chai"
-
-const randomPrivateKey = () => {
-  let privateKey
-  do {
-    privateKey = anchor.web3.Keypair.generate().secretKey.slice(0, 32)
-  } while (!privateKeyVerify(privateKey))
-  return privateKey
-}
 
 type Message = {
   subject: PublicKey
   expiration: anchor.BN
+  schema: string
 }
 
 const LAYOUT: borsh.Layout<Message> = borsh.struct([
@@ -45,22 +38,22 @@ describe("verity", () => {
   })
 
   it("Is initialized!", async () => {
-    const tx = await program.rpc.initialize({})
+    await program.rpc.initialize({})
   })
 
   it("passes if verified", async () => {
-    // Eth Address (Verifier)
-    const privateKey = Buffer.from([
+    // Verifier:
+    // This key will be used to sign the verification result
+    const verifier = Buffer.from([
       203, 130, 240, 71, 57, 83, 172, 48, 120, 59, 41, 88, 207, 209, 1, 232,
       252, 67, 245, 125, 108, 162, 166, 116, 133, 157, 170, 245, 177, 84, 59, 1
     ])
-    const publicKey = publicKeyCreate(privateKey, false).slice(1)
-    const ethAddress = Secp256k1Program.publicKeyToEthAddress(publicKey)
 
     // Alice will be the subject
     const alice = anchor.web3.Keypair.generate()
 
-    // Setup message contents
+    // Setup message contents. Solana uses unix timestamp for clock time,
+    // whereas javascript uses milliseconds.
     const expiration = new anchor.BN(Date.now() / 1000 + 6000)
     const data = {
       subject: alice.publicKey,
@@ -69,13 +62,15 @@ describe("verity", () => {
     }
 
     // Allocate buffer for the message and borsh encode the data
+    // The example program allows for messages up to 128 bytes.
     const message = Buffer.alloc(128)
     LAYOUT.encode(data, message)
 
     // Create Signature from the message
     const messageHash = Buffer.from(keccak_256.update(message).digest())
-    const { signature, recid: recoveryId } = ecdsaSign(messageHash, privateKey)
+    const { signature, recid: recoveryId } = ecdsaSign(messageHash, verifier)
 
+    // Make RPC call to verify
     await program.rpc.verify(signature, recoveryId, message, {
       accounts: {
         subject: alice.publicKey,
@@ -85,7 +80,7 @@ describe("verity", () => {
     })
   })
 
-  it("borsh", async () => {
+  it("borsh encoded messages can be decoded", async () => {
     const keypair = anchor.web3.Keypair.generate()
     const expiration = new anchor.BN(Date.now())
 
@@ -101,5 +96,6 @@ describe("verity", () => {
     const decoded = LAYOUT.decode(buffer)
     expect(decoded.subject.equals(keypair.publicKey)).to.be.true
     expect(decoded.expiration.eq(expiration)).to.be.true
+    expect(decoded.schema).to.eq("hello, world")
   })
 })
