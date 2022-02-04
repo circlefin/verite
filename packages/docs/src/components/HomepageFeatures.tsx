e4ef0211e2b9fc1dcf6316c035ab1ebf3b23feb1
Matt Venables <matt@m2.xyz>
2022-02-04 13:08:10 -0500

    Fix fonts for Docs package

--- packages/docs/src/components/HomepageFeatures.tsx
+++ packages/docs/src/components/HomepageFeatures.tsx
@@ -39,12 +39,12 @@ const FeatureList = [
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx("col col--4 feature-card")}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
-        <h3 className="feature-card-title">{title}</h3>
+        <h3>{title}</h3>
         <p>{description}</p>
       </div>
     </div>
