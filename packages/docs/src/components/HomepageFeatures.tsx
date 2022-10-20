e14da2991bb809e1ead9ab5cc50dbeee4916eb47
bumblefudge <jcaballero@centre.io>
2022-10-20 12:45:55 +0200

    to fix JSX syntax error

--- packages/docs/src/components/HomepageFeatures.tsx
+++ packages/docs/src/components/HomepageFeatures.tsx
@@ -7,34 +7,31 @@ const FeatureList = [
   {
     title: "Issuers give value and freedom to their customers",
     description: (
-      <>
+      <><>
         Offering verifiable, portable, context-independent, off-chain "membership cards" strengthens a customer relationship and an issuer's brand.
-      </>
-      <Link className="button button--secondary button--lg" to="/verite/issuers/getting-started">
-        Issuer Overview
-      </Link>
+      </><Link className="button button--secondary button--lg" to="/verite/issuers/getting-started">
+          Issuer Overview
+        </Link></>
     ),
   },
   {
     title: "Wallets empower their users by supporting these flows",
     description: (
-      <>
+      <><>
         If wallets are truly going to be the "browsers" of Web3, wallets need meaningful consent and privacy. Verite offers a minimum-viable, incremental path towards supporting these emerging patterns and development styles.
-      </>
-      <Link className="button button--secondary button--lg" to="/verite/wallets/getting-started">
-      Wallet Overview
-    </Link>
+      </><Link className="button button--secondary button--lg" to="/verite/wallets/getting-started">
+          Wallet Overview
+        </Link></>
   ),
   },
   {
     title: "Verifiers unlock wider, safer customer bases for dapps",
     description: (
-      <>
+      <><>
         Faster, smoother onboarding and tapping into existing customer bases is key to getting more and better users; standards and herd privacy are key to doing so without deanonymizing them or discriminating one wallet at a time.
-      </>
-      <Link className="button button--secondary button--lg" to="/verite/issuers/getting-started">
-        Issuer Overview
-      </Link>
+      </><Link className="button button--secondary button--lg" to="/verite/issuers/getting-started">
+          Issuer Overview
+        </Link></>
     
     ),
   },
