53f23a05eab5c86e84c81da58c931a153084ff1a
Kim Duffy <kimdhamilton@gmail.com>
2023-03-02 19:28:57 -0800

    claim format test

---
+++ packages/verite/test/lib/utils/claim-format.test.ts
@@ -0,0 +1,13 @@
+import {  parseClaimFormat } from "../../../lib/utils"
+
+describe("claim-format", () => {
+  it("works", async () => {
+    const result = parseClaimFormat( {
+      jwt_vp: {
+        alg: ["EdDSA"]
+      }
+    })
+    expect(result).toEqual("jwt_vp")  
+
+  })
+})
